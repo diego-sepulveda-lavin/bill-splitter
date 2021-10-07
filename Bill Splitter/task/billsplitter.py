f5ec72c8192b00991cf4d95ef0f12f0b08a5c95e
Diego Sepulveda <diegosl@hotmail.cl>
2021-10-07 20:09:07 -0300

    task 4/4 done

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,35 +1,62 @@
 from random import choice
+
+
+def lucky_winner(luck_enabled, payment_detail):
+    luck_enabled = luck_enabled.lower()
+
+    if luck_enabled == 'no' or luck_enabled == 'n':
+        print("No one is going to be lucky")
+        return None
+    elif luck_enabled == "yes" or luck_enabled == 'y':
+        participants = []
+        for friend_name in payment_detail:
+            participants.append(friend_name)
+        winner = choice(participants)
+        print(f"{winner} is the lucky one!")
+        return winner
+    else:
+        print('Incorrect choice. "NO" is assumed')
+        return None
+
+
+def calculate_bill(friends_number, payment_detail, total, winner):
+    if winner:
+        friends_number -= 1
+    payment_per_person = round(total / friends_number, 2)
+
+    for friend in payment_detail:
+        if winner == friend:
+            continue
+        payment_detail[friend] = payment_per_person
+
+
+def generate_empty_bill(friends_number):
+    payment_detail = {}
+    for i in range(friends_number):
+        name = input()
+        payment_detail[name] = 0
+    return payment_detail
+
+
 try:
     print("Enter the number of friends joining (including you):")
-    people_qty = int(input())
-    people = {}
-    if people_qty < 1:
+    friends_qty = int(input())
+    if friends_qty < 1:
         print("No one is joining for the party")
     else:
         print("Enter the name of every friend (including you), each on a new line:")
-        for i in range(people_qty):
-            name = input()
-            people[name] = 0
+        bill_detail = generate_empty_bill(friends_qty)
 
         print("Enter the total bill value:")
         bill_total = int(input())
-        bill_per_person = round(bill_total / people_qty, 2)
-
-        for person in people:
-            people[person] = bill_per_person
 
         print('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
         luck_feature = input()
-        if luck_feature == 'No':
-            print("No one is going to be lucky")
-        elif luck_feature == "Yes":
-            lucky_names = []
-            for name in people:
-                lucky_names.append(name)
-            winner = choice(lucky_names)
-            print(f"{winner} is the lucky one!")
-        else:
-            print('Incorrect choice. "NO" is assumed')
+
+        lucky_one = lucky_winner(luck_feature, bill_detail)
+        calculate_bill(friends_qty, bill_detail, bill_total, lucky_one)
+
+        print(bill_detail)
 
 except ValueError:
     print("Please enter a valid number")
